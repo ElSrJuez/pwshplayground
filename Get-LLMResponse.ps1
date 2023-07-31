f5bb3f5b1d443e338c7084f82bf848cbd765c851
Diego Vasquez <dvasquez@unicef.org>
2023-07-31 10:32:26 +0200

    structuring the function...

--- Get-LLMResponse.ps1
+++ Get-LLMResponse.ps1
@@ -1,7 +1,7 @@
 function Get-LLMResponse {
     param (
-        [string]$userPrompt = "I want to create a CV but I dont know how, so: come up with 6 questions that will but excellent CV for me. Ready? Ask question one:",
-        [string]$SystemPrompt = "You are an assistant that helps creating content based on a sequence of questions that you ask and answers the user provides. Your questions must be simple YES/NO or simple multiple-choice. You will ask one question and wait for one answer, only then you will ask the next question. For maximum efficiency, you will desigin each next question to maximize the knowledge you accumulate about the subject. Afer I answer your last question you will output a rich and meaningul seamless document based on the original request of the user.",
+        [string]$userPrompt,
+        [string]$SystemPrompt = "You are an assistant that helps creating content based on a sequence of questions that you ask and answers the user provides. You will ask one question and wait for one answer, only then you will ask the next question. For maximum efficiency, your questions must be simple YES/NO or simple multiple-choice, do not ask open questions. You will design each next question to maximize the knowledge you accumulate about the subject. Afer the user answers your last question you will output a rich and meaningul seamless document based on the original request of the user and you will start that last response with the string ---COMPLETE---.",
         [string]$TokenValue,
         $PreviousObject,
         [Int32]$MaxToken = 8000,
@@ -9,14 +9,6 @@ function Get-LLMResponse {
         [string]$ModelURI = "https://oa-ucefdev-openai-2.openai.azure.com/openai/deployments/ucefdev-language-16k/chat/completions?api-version=2023-03-15-preview"
     )
 
-    $PreviousObject | Out-Host
-
-    if ($PSBoundParameters.ContainsKey('PreviousObject')) {
-        Remove-Variable h -ErrorAction SilentlyContinue
-        <# $h = @{}
-        $PreviousObject.psobject.properties | 
-            ForEach-Object { $h[$_.Name] = $_.Value }
-        #>
         $messages = @()
         $SystemHash = @{
                 "role" = "system"  
@@ -24,22 +16,18 @@ function Get-LLMResponse {
         }
         $messages += $SystemHash
 
+        if ($PSBoundParameters.ContainsKey('PreviousObject')) {
+            $messages += $PreviousObject
+        }
+        
         $usermessage = @{
             "role" = "user"
             "content"= $userPrompt
         }
         $messages += $usermessage
-        $messages += $PreviousObject
-        
+
         $body = @{  
-            "messages" = @(  
-                @{  
-                    "role" = "system"  
-                    "content" = $SystemPrompt 
-                },
-                $usermessage,
-                $PreviousObject
-            )  
+            "messages" = $messages
             "max_tokens" = $MaxToken  
             "temperature" = 0.5  
             "frequency_penalty" = 0  
@@ -48,23 +36,7 @@ function Get-LLMResponse {
             "stop" = $null  
         } | ConvertTo-Json 
         
-    } else {
-        $body = @{  
-            "messages" = @(  
-                @{  
-                    "role" = "system"  
-                    "content" = $SystemPrompt 
-                },
-                $usermessage
-            )  
-            "max_tokens" = $MaxToken  
-            "temperature" = 0.5  
-            "frequency_penalty" = 0  
-            "presence_penalty" = 0  
-            "top_p" = 0.95  
-            "stop" = $null  
-        } | ConvertTo-Json 
-    }
+        $body | Out-Host
 
     $aiHeaders = @{
         'Authorization' = "$($TokenType) $($TokenValue)"
@@ -74,39 +46,21 @@ function Get-LLMResponse {
     $body | Out-Host
     $apiResponse = Invoke-RestMethod -Method Post -Uri $ModelURI -Headers $aiHeaders -Body $body
 
-    Remove-Variable ho -ErrorAction SilentlyContinue
+    Remove-Variable ho,myOutput -ErrorAction SilentlyContinue
+    $myOutput = @()
     $ho = @{}
+    if ($PSBoundParameters.ContainsKey('PreviousObject')) {
+        $myOutput += $PreviousObject
+    }
+    $myOutput += $usermessage
+    
     $apiResponse.choices.message.psobject.properties | 
         ForEach-Object { $ho[$_.Name] = $_.Value }
-    if ($PSBoundParameters.ContainsKey('PreviousObject')) {
-        $out =  @(
-                $usermessage,
-                $ht,
-                $ho
-            )
-    } else {
-        $out = @(
-                $usermessage,
-                $ho
-        )
-    
-    }
-    $out
+
+    $myOutput += $ho 
+    $myOutput
 }
 
-
-
-Connect-AzAccount -UseDeviceAuthentication
-
-$aiToken = Get-AzAccessToken -ResourceUrl 'https://cognitiveservices.azure.com'
-
-
-$r1 = Get-LLMResponse  -TokenValue $aiToken.Token
-<#
-$h1 = @{}
-$r1.psobject.properties | 
-    ForEach-Object { $h1[$_.Name] = $_.Value }
-#>
 function Find-LastAssistantMessage {
     param ($LLMOutput)
 
@@ -120,10 +74,33 @@ function Find-LastAssistantMessage {
     $j
 }
 
-$x = Find-LastAssistantMessage $r1
-$a1 = Read-Host "$($x)" 
+Connect-AzAccount -UseDeviceAuthentication
+
+$aiToken = Get-AzAccessToken -ResourceUrl 'https://cognitiveservices.azure.com'
+
+$CreateCVPropmpt = "I want to create a CV but I dont know how, so: come up with 6 questions that will build an excellent CV for me. Do not ask me contact details, as I will complete these later. Ready? Ask question one:"
+
+
+Remove-Variable r, x -ErrorAction SilentlyContinue
+$a = $CreateCVPropmpt
+while ($x -notlike "*---COMPLETE---*") {
+    if ($r) {
+        $r = Get-LLMResponse  -TokenValue $aiToken.Token -userPrompt $a -PreviousObject $r
+    } else {
+        $r = Get-LLMResponse  -TokenValue $aiToken.Token -userPrompt $a
+    }
+    cls
+    $x = Find-LastAssistantMessage $r
+    $a = Read-Host "$($x)" 
+}
+
 
 $r2 = Get-LLMResponse -TokenValue $aiToken.Token -PreviousObject $r1 -userPrompt $a1 
+
+$x = Find-LastAssistantMessage $r2
+$a2 = Read-Host "$($x)" 
+
+$r3 = Get-LLMResponse -TokenValue $aiToken.Token -PreviousObject $r2 -userPrompt $a2
 $h2 = @{}
 $r2.psobject.properties | 
     ForEach-Object { $h2[$_.Name] = $_.Value }
