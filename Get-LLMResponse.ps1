88530b5b7578a0a0ad3b6ad4dde38c6db4a5dcae
Diego Vasquez <elsrjuez@live.com>
2023-07-31 12:08:59 +0200

    maybe first workable version

--- Get-LLMResponse.ps1
+++ Get-LLMResponse.ps1
@@ -10,11 +10,11 @@ function Get-LLMResponse {
         [string]$TokenValue,
         [Parameter(mandatory=$false)]
         $PreviousObject,
-        [string]$SystemPrompt = "You are an assistant that helps creating content based on a sequence of questions that you ask and answers the user provides. You will ask one question and wait for one answer, only then you will ask the next question. For maximum efficiency, your questions must be simple YES/NO or simple multiple-choice, do not ask open questions. You will design each next question to maximize the knowledge you accumulate about the subject. Afer the user answers your last question you will output a rich and meaningul seamless document based on the original request of the user and you will start that last response with the string ---COMPLETE---.",
+        [string]$SystemPrompt = 'You are a chat-type assistant that will answer open questions.',
+        [Parameter(mandatory=$false)]
         [Int32]$MaxToken = 8000,
         [string]$TokenType = 'Bearer',
-        [string]$ModelURI = "https://oa-ucefdev-openai-2.openai.azure.com/openai/deployments/ucefdev-language-16k/chat/completions?api-version=2023-03-15-preview",
-        [switch]$Verbose
+        [string]$ModelURI = "https://oa-ucefdev-openai-2.openai.azure.com/openai/deployments/ucefdev-language-16k/chat/completions?api-version=2023-03-15-preview"
     )
 
     # init request elements
@@ -97,74 +97,33 @@ Connect-AzAccount -UseDeviceAuthentication
 $aiToken = Get-AzAccessToken -ResourceUrl 'https://cognitiveservices.azure.com'
 
 # Demo user prompt
-$CreateCVPropmpt = "I want to create a CV but I dont know how, so: come up with 6 questions that will build an excellent CV for me. Do not ask me contact details, as I will complete these later. Ready? Ask question one:"
+$CreateCVSystem = "You are an assistant that helps creating content based on a sequence of questions that you ask and answers the user provides. You will ask one question and wait for one answer, only then you will ask the next question. You will design each next question to maximize the knowledge you accumulate about the subject. After the user answers your last question you will output a rich and meaningul seamless document based on the original request of the user and you will start that last response with the string ---COMPLETE---. Before each question you will inform the user a rough estimate how much information you think is needed the user has already provided in the format of `"You have answered x number of questions, I think with y more questions The information you have provided will be enough`". For maximum efficiency, ALWAYS ask in the form of simple multiple-choice prompting using letters for each choice. NEVER ask open-ended questions."
+$CreateCVPrompt = "I want to create a CV but I dont know how, so: come up with a number of questions and when you have enough information, build a complete rich CV. The name and contact information should be in the last question. Ready? Ask question one:"
 
 # loop until complete response.
-Remove-Variable r, x -ErrorAction SilentlyContinue
-$a = $CreateCVPropmpt
-while ($x -notlike "*---COMPLETE---*") {
+Remove-Variable r -ErrorAction SilentlyContinue
+$exit = $false
+$a = $CreateCVPrompt
+[string[]]$x = ''
+do {
     if ($r) {
-        $r = Get-LLMResponse  -TokenValue $aiToken.Token -userPrompt $a -PreviousObject $r -ver
+        $r = Get-LLMResponse -TokenValue $aiToken.Token -userPrompt $a -PreviousObject $r -SystemPrompt $CreateCVSystem
     } else {
-        $r = Get-LLMResponse  -TokenValue $aiToken.Token -userPrompt $a
+        $r = Get-LLMResponse -TokenValue $aiToken.Token -userPrompt $a -SystemPrompt $CreateCVSystem
     }
     cls
-    $x = Find-LastAssistantMessage $r
-    $a = Read-Host "$($x)" 
-}
-
-
-$r2 = Get-LLMResponse -TokenValue $aiToken.Token -PreviousObject $r1 -userPrompt $a1 
-
-$x = Find-LastAssistantMessage $r2
-$a2 = Read-Host "$($x)" 
-
-$r3 = Get-LLMResponse -TokenValue $aiToken.Token -PreviousObject $r2 -userPrompt $a2
-$h2 = @{}
-$r2.psobject.properties | 
-    ForEach-Object { $h2[$_.Name] = $_.Value }
-$h = @{
-    $h1,
-    $h2
-}
-
-#$response = Invoke-WebRequest -Uri $aiuri -Method POST -Headers $authHeader -Body $body  
-
-| Out-Host
-
-
-        $codeMessage = @{
-    "role" = "user"
-    "content"= $AssistantResponse | ConvertTo-Json
+    Write-Host "Type EXIT to quit."
+    IF ($r) {
+        $x = Find-LastAssistantMessage $r
     }
-$CommentPrompt = "You are an API function that takes an input block of text from the user that may contain programming or scripting code, or a combination of text an code. You will first detect the programming or scripting language of the code. Then you will convert any non-runnable text or comments into syntactically and contextually correct code in the format of the original language."
-$bodyCode = @{  
-    "messages" = @(  
-        @{  
-            "role" = "system"  
-            "content" = $CommentPrompt
-        },
-        $codeMessage 
-    )  
-    "max_tokens" = $MaxToken  
-    "temperature" = 0.3  
-    "frequency_penalty" = 0  
-    "presence_penalty" = 0  
-    "top_p" = 0.95  
-    "stop" = $null  
-} | ConvertTo-Json -Compress
-
-$apiCodeResponse = Invoke-RestMethod -Method Post -Uri $aiuri -Headers $aiHeaders -Body $bodyCode
-
-$runnableCode = $apiCodeResponse.choices.message.content 
-
-Write-Host "Now I will do what no human should allow ever - to run AI-generated code." -ForegroundColor Yellow -BackgroundColor Red
-write-host 
-$runnableCode | Out-Host
-Write-Host "Remember SKYNET!!" -ForegroundColor Yellow -BackgroundColor Red
-write-host 
-$answer = Read-Host "Type YES to save and run the code block above:" 
-if ($answer = 'YES') {
-    $runnableCode | Out-File $outputCodeFileName 
-    & .\$outputCodeFileName
-}
+    else {
+        write-host "LLM Response was null or unexpected result, exiting." -ForegroundColor Yellow
+        $exit = $true
+    }
+    if ($x) {
+        $a = Read-Host "$($x)"
+        if ($a -eq 'EXIT') {
+            $exit = $true
+        }   
+    }
+} until ([Boolean]($x | Select-String "---COMPLETE---" -SimpleMatch) -or $exit) 
