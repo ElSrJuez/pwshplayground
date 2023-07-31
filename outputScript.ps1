77514e22d1450c8fc06a7384a76e308f1b8e3dd4
Diego Vasquez <elsrjuez@live.com>
2023-07-31 13:27:32 +0200

    cleaned up yet even more

--- outputScript.ps1
+++
@@ -1,9 +0,0 @@
-# Sure! Here's the equivalent PowerShell code for the given BASIC code:
-
-#```powershell
-while ($true) {
-    # Write-Host "HELLO WORLD!"
-}
-#```
-
-# In PowerShell, you can achieve an infinite loop by using a `while` loop with a condition that is always true (`$true`). The `Write-Host` cmdlet is used to print the "HELLO WORLD!" message to the console.
